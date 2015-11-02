0b949eb5e3c6de8281eb6a184ba5ae17ffbf9274
lshumlich <larry.shumlich@thesolutionstack.com>
2015-11-01 19:17:39 -0700

    - initial version

---
+++ excel-app/Coverage.bat
@@ -0,0 +1,4 @@
+rem
+rem This will gather the statistics of running our code
+rem
+python -m coverage run CalcRoyalties.py DataBase.xlsx
---
+++ excel-app/CoverageReport.bat
@@ -0,0 +1,4 @@
+rem
+rem This will report on the statistics of the last coverage run
+rem
+python -m coverage report -m
