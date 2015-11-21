4718812bb69fa41d24d60bc7db88f451fbfd4b4d
lshumlich <larry.shumlich@thesolutionstack.com>
2015-11-20 23:31:13 -0700

    - restructure the parsing module

--- excel-app/ocrscan.py
+++ excel-app/ocrscan.py
@@ -7,69 +7,134 @@ import re
 LeaseType    LeaseID    RightsGranted    RoyaltyScheme    CrownMultiplier    MinRoyalty    ValuationMethod    TruckingDeducted    ProcessingDeducted    Notes
 OL    1    All    SKProvCrownVar    1.2    50    SaskWellHead    Y    Y    
 """
-def lookForString (sToLookIn,pattern,result,sToAppend):
-        m = pattern.search(sToLookIn)
-        if m != None:
-            if result != '':
-                result +=','
-            result += sToAppend
-        return result
 
-
-leasenoCol=0 # A
-royschemCol=13 # N
-gorrCol=46 # AU
-leaseRightsCol=12 # M
-appBDetailCol=39 # AN
-
-regulationP = re.compile('regulation royalty rate',re.I)
-crownVar1P = re.compile('multiples of provincial',re.I)
-crownVar2P = re.compile('modified provincial crown',re.I)
-gorrP = re.compile('gorr',re.I)
-percentP = re.compile('[0-9.]+%')
-
-
-leaseWorksheetName ="C://Temp/SK Lease Data - All.xlsx"
-
-leasewb = load_workbook(leaseWorksheetName)
-ws = leasewb.active
-
-headerRow = None
-i = 0
- 
-for row in ws.rows:
-    i += 1
-    if headerRow == None:
-        headerRow = row
-        print('---Col:', row[leaseRightsCol].value)
-        print('---Col:', row[appBDetailCol].value)
-    else:
-        lease = row[leasenoCol].value
-        leaseType = lease [0:2]
-        leaseID = int (lease [3:9])
-        leaseRights =  row[leaseRightsCol].value
-        appBDetail = row[appBDetailCol].value
+class ParseLease(object):
+    
+    def __init__(self,getter):
+        self.getCellValue = getter
+    
+    def leaseType(self):
+        lease = self.getCellValue('Leaseno')
+        return lease [0:2]
+        
+    def leaseID(self):
+        lease = self.getCellValue('Leaseno')
+        return int (lease [3:9])
+    
+    def leaseRights(self):
+        leaseRights = self.getCellValue('Lsrights')
         rightsGranted = "*** Research Required ***"
         if leaseRights == "Oil & Gas [Excluding Crude Bitumen]":
-           rightsGranted = "O+G-Cbit"
+            rightsGranted = "O+G-Cbit"
         if leaseRights == "GasOnlyOilOnly":
-           rightsGranted = "O+G-Cbit"
-           
-           
+            rightsGranted = "O+G-Cbit"
+        return rightsGranted
+    
+    def oilMultiplier(self):
+        oilMultiplierP = re.compile('oil royalty rate means ([0-9.]+) times', re.I)
+        appBDetail = self.getCellValue('Royappb')
+        oilMultiplier = None
+        if (appBDetail != None):
+            oilMultiplier = '*** Research Required ***'
+            m = oilMultiplierP.search(appBDetail)
+            if m != None:
+                oilMultiplier = m.group(1)
+        return oilMultiplier
 
-        
-        s = row[royschemCol].value
-        royaltyScheme = lookForString(s,regulationP,'','Regulation1995')
-        royaltyScheme = lookForString(s,crownVar1P,royaltyScheme,'SKProvCrownVar')
-        royaltyScheme = lookForString(s,crownVar2P,royaltyScheme,'SKProvCrownVar')
-        royaltyScheme = lookForString(s,gorrP,royaltyScheme,'GORR')
+    def royaltyScheme(self):
+        regulationP = re.compile('regulation royalty rate',re.I)
+        crownVar1P = re.compile('multiples of provincial',re.I)
+        crownVar2P = re.compile('modified provincial crown',re.I)
+        gorrP = re.compile('gorr',re.I)
+        s = self.getCellValue('Royschem')
+        royaltyScheme = ''
+        if s != None:
+            royaltyScheme = self.lookForString(s,regulationP,royaltyScheme,'IOGR1995')
+            royaltyScheme = self.lookForString(s,crownVar1P,royaltyScheme,'SKProvCrownVar')
+            royaltyScheme = self.lookForString(s,crownVar2P,royaltyScheme,'SKProvCrownVar')
+            royaltyScheme = self.lookForString(s,gorrP,royaltyScheme,'GORR')
         if royaltyScheme == '':
-            royaltyScheme += '???Unknown???'
+            royaltyScheme += '*** Research Required ***'
+        return royaltyScheme
 
-        gorrStuff = ''
-        if 'GORR' in royaltyScheme:
-            s = row[gorrCol].value
+    def gorr(self):
+        percentP = re.compile('[0-9.]+%')
+        s = self.getCellValue('Gorr')
+        gorrStuff = None
+        if s != None:
             gorrStuff = percentP.findall(s)
+        return gorrStuff
         
-        print(i,lease, leaseType, leaseID, leaseRights, rightsGranted, royaltyScheme,appBDetail, gorrStuff)
+    def lookForString (self,sToLookIn,pattern,result,sToAppend):
+            m = pattern.search(sToLookIn)
+            if m != None:
+                if result != '':
+                    result +=','
+                result += sToAppend
+            return result
+
+class MultiLineWS(object):
+
+    def getCellValue(self,label):
         
+        i = 0
+        for cell in self.headerRow:
+            if cell.value == label:
+                return self.row[i].value
+            i += 1
+        raise Exception('Cell ' + label + ' not found.')
+             
+    def process(self,wsName):
+        
+        parseLease = ParseLease(self.getCellValue)
+        
+        leasewb = load_workbook(wsName)
+        ws = leasewb.active
+        
+        self.headerRow = None
+        i = 0
+         
+        for self.row in ws.rows:
+            i += 1
+            if self.headerRow == None:
+                self.headerRow = self.row
+            else:
+                print('row:', i, 'LeaseType:',parseLease.leaseType(),'leaseID:',parseLease.leaseID(),
+                      'leaseRights:',parseLease.leaseRights(),
+                      'oilMultiplier:',parseLease.oilMultiplier(),
+                      'royaltyScheme:',parseLease.royaltyScheme(),
+                      'gorr:',parseLease.gorr())
+    
+
+class SingeLeaseWS(object):
+
+    def getCellValue(self,label):
+        
+        for row in self.ws.rows:
+            if row[0].value == label:
+                return row[1].value
+        raise Exception('Cell ' + label + ' not found.')
+             
+    def process(self,wsName):
+        
+        parseLease = ParseLease(self.getCellValue)
+        
+        leasewb = load_workbook(wsName)
+        self.ws = leasewb.active
+
+        print('LeaseType:',parseLease.leaseType())
+        print('leaseID:',parseLease.leaseID())
+        print('leaseRights:',parseLease.leaseRights())
+        print('oilMultiplier:',parseLease.oilMultiplier())
+        print('royaltyScheme:',parseLease.royaltyScheme())
+        print('gorr:',parseLease.gorr())
+#
+# Sample of running with the Multi Line WS        
+#
+multiLine = MultiLineWS()
+multiLine.process(r"D:\code\info\Lease Analysis - Sask Only.xlsx")
+#
+# Sample of running with Single Lease WS
+#
+singleLease = SingeLeaseWS()
+singleLease.process(r"D:\code\info\book2.xlsx")
--- excel-app/play.py
+++ excel-app/play.py
@@ -1,6 +1,22 @@
 import re
 import urllib.request as req
 
+text = """
+"3.0 ROYALTY 
+3.1 Subject to clause 3.2, the gross royalty on Oil or Gas and Products produced from or attributable to the Lease Lands shall be calculated in accordance with this Appendix, and the attached Exhibit 'A"". 
+3.2 Royalty on Condensate shall be calculated in accordance with the Pentanes Plus royalty prescribed in this Lease.
+A. OIL ROYALTY The gross royalty for Oil shall be the Oil Royalty Rate multiplied by the Oil Price multiplied by the Oil Royalty Production for that month, where;
+Oil Royalty Rate means 1.0 times the prevailing Province of Saskatchewan Crown Oil Royalty rate for fourth tier oil as prescribed by the Crown Royalty Legislation [without reference to royalty holidays or other special incentives]; 
+The net royalty payable on Oil shall be the gross royalty on the Oil less the Trucking Costs for Oil. 
+B. GAS ROYALTY 
+The gross royalty for Gas shall be the Gas Production for that month multiplied by the Gas Royalty Rate multiplied by the Gas Price for that month, where; 
+Gas Royalty Rate means 1.0 times the prevailing Province of Saskatchewan Crown Royalty rate for fourth tier gas, as prescribed by the Crown Royalty Legislation, [without reference to royalty holidays or other special incentives]; 
+The net royalty payable on Gas shall be the gross royalty on Gas less the Gas Cost Allowance.
+Notwithstanding clause 4.0 in Appendix ""B"" - Royalty Terms, the Authorized Deduction for Gas Cost Allowance will be as per the Saskatchewan Crown Royalty Legislation to a maximum of $10.00/10 3m3 [ten dollars per thousand cubic metres]."
+"""
+
+
+
 class CR(object):
     def writeLog(self):
         s = ' asdf asdf asdf asdf\n'
@@ -69,3 +85,36 @@ print()
 query = '#$%asdf-_+.txt'
 q = req.pathname2url(query)
 print (q)
+
+s = 'asdf asdf asdf sadf Oil Royalty Rate means 1.0 times the prevailing '
+
+p = re.compile('oil royalty rate means ([0-9.]+) times', re.I)
+print(s)
+print(p)
+m = p.search(s)
+print(m)
+print(m.group(0))
+print(m.group(1))
+t = float(m.group(1))
+x = t * 3
+print(x)
+print('-----')
+
+s = '''
+<div id=hotlinklist>
+<a href="foo1.com">Foo1</a>
+  <div id=hotlink>
+    <a href="/">Home</a>
+  </div>
+  <div id=hotlink>
+    <a href="/extract">Extract</a>
+  </div>
+  <div id=hotlink>
+    <a href="/sitemap">Sitemap</a>
+  </div>
+</div>'''
+m = re.compile(r'<a href="/sitemap">(.*?)</a>').search(s)
+print(m)
+print(m.group(0))
+print(m.group(1))
+
