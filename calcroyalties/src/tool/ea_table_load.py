a5b460d282bb96bf5bed32711fbf09b714dc7ac7
lshumlich <larry.shumlich@thesolutionstack.com>
2016-04-11 13:08:35 -0600

    - Load the tables in EA

--- calcroyalties/src/tool/ea_table_load.py
+++ calcroyalties/src/tool/ea_table_load.py
@@ -3,9 +3,13 @@ import xml.etree.ElementTree as ET
 from xml.etree import ElementTree
 from xml.dom import minidom
 
+import config
+
 class EA_Tables (object):
 
     def __init__(self):
+        self.table_count = 0
+        self.attribute_count = 0
 
         self.root = ET.Element('XMI')
         self.root.set('xmi.version', '1.2')
@@ -14,25 +18,22 @@ class EA_Tables (object):
         e = ET.SubElement(e, 'UML:Model')
         e.set('xmi.id', 'M.1')
         e.set('name', 'Lorraine')
-        e = ET.SubElement(e, 'UML:Namespace.ownedElement')
+        self.element = ET.SubElement(e, 'UML:Namespace.ownedElement')
 
-        t = ET.SubElement(e, 'UML:Class')
-        t.set('xmi.id', 'C.1')
-        t.set('name', 'LorraineTable')
+    def table(self,table):
+        self.table_count += 1
+        t = ET.SubElement(self.element, 'UML:Class')
+        t.set('xmi.id', 'C.' + str(self.table_count))
+        t.set('name', table)
 
-        t = ET.SubElement(t, 'UML:Classifier.feature')
+        self.tab = ET.SubElement(t, 'UML:Classifier.feature')
 
-        a = ET.SubElement(t, 'UML:Attribute')
-        a.set('xmi.id', 'A.1')
-        a.set('name', 'Name')
+    def attribute(self,attribute):
+        self.attribute_count += 1
 
-        a = ET.SubElement(t, 'UML:Attribute')
-        a.set('xmi.id', 'A.2')
-        a.set('name', 'Street')
-
-        a = ET.SubElement(t, 'UML:Attribute')
-        a.set('xmi.id', 'A.3')
-        a.set('name', 'Zip')
+        a = ET.SubElement(self.tab, 'UML:Attribute')
+        a.set('xmi.id', 'A.' + str(self.attribute_count))
+        a.set('name', attribute)
 
     def prettify(self):
         """Return a pretty-printed XML string for the Element.
@@ -45,5 +46,22 @@ class EA_Tables (object):
 
 ea = EA_Tables()
 
+dbi = config.get_database_instance()
 
-# print(ea.prettify())
+tables = dbi.get_table_names()
+for t in tables:
+    ea.table(t)
+
+    c = dbi.execute("PRAGMA table_info(" + t + ");")
+
+    for row in c:
+        ea.attribute(row[1])
+        # print(row,row[1],row[2])
+
+txt = ea.prettify()
+
+f = open(config.get_temp_dir() + 'ea.xml', 'w')
+f.write(txt)
+f.close()
+
+print(txt)
