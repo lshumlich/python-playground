d38bbe50654ed2127b34c0c6df1358ecdb9f1042
Konstantin Lazutin <konstantin.lazutin@thesolutionstack.com>
2016-04-25 14:11:13 -0600

    Some more view crosslinks

--- calcroyalties/src/app/views/wellevent.py
+++ calcroyalties/src/app/views/wellevent.py
@@ -8,20 +8,23 @@ wellevent = Blueprint('wellevent', __name__)
 @wellevent.route('/wellevent/search', methods=['GET'])
 def search():
     if not request.args: return render_template('wellevent/search.html')
-    # values = {'Meridian': 'WellEvent', 'WellEvent'}
-    # for arg in request.args:
-    #     if arg in values:
-    #         arg = "WellEvent."+arg
-
     db = config.get_database()
     statement = """SELECT WellEventInfo.WellEvent, WellEventInfo.Meridian, RTAHeader.RTPOperator, WellEventStatus.Status, BAInfo.CorpShortName
 	FROM WellEventInfo, RTAHeader, WellEventStatus, BAInfo
 	WHERE DATE('2016-04-01') BETWEEN WellEventInfo.StartDate AND WellEventInfo.EndDate AND WellEventInfo.WellEvent=RTAHeader.WellEvent
 	AND DATE('2016-04-01') BETWEEN WellEventStatus.StartDate AND WellEventStatus.EndDate AND WellEventInfo.WellEvent=WellEventStatus.WellEvent
 	AND DATE('2016-04-01') BETWEEN WellEventStatus.StartDate AND WellEventStatus.EndDate
-	AND RTAHeader.RTPOperator=BAInfo.BAid AND DATE('2016-04-01') BETWEEN BAInfo.StartDate AND BAInfo.EndDate
-    """
-    results = db.select_sql(statement)
+	AND RTAHeader.RTPOperator=BAInfo.BAid AND DATE('2016-04-01') BETWEEN BAInfo.StartDate AND BAInfo.EndDate"""
+
+    argument_tables = {'Meridian': 'WellEventInfo', 'WellEvent': 'WellEventInfo'}
+    kwargs = dict((k, v) for k, v in request.args.items() if v)  # this is to get rid of empty values coming from forms
+    search_arguments = ""
+    for arg in kwargs:
+        if arg in argument_tables:
+            compound = argument_tables[arg] + '.' + arg + '=' + '"' + kwargs[arg] + '"'
+            search_arguments += " AND " + compound
+
+    results = db.select_sql(statement + search_arguments)
     if results:
         return render_template('wellevent/search.html', results=results, search_terms=request.args.to_dict())
     else:
@@ -38,8 +41,11 @@ def details(wellevent_num):
     # try:
     db = config.get_database()
     wellevent = db.select1('WellEventInfo', WellEvent=wellevent_num)
-    statement = """SELECT Lease.* FROM Lease, WellLeaseLink WHERE WellLeaseLink.WellEvent="%s" AND Lease.ID=WellLeaseLink.LeaseID"""
-    leases = db.select_sql(statement % wellevent_num)
-    return render_template('wellevent/details.html', wellevent=wellevent, leases=leases)
+    statement_leases = """SELECT Lease.* FROM Lease, WellLeaseLink WHERE WellLeaseLink.WellEvent="%s" AND Lease.ID=WellLeaseLink.LeaseID"""
+    leases = db.select_sql(statement_leases % wellevent_num)
+    statement_facilities = """SELECT FacilityInfo.* FROM FacilityInfo, WellFacilitylink WHERE FacilityInfo.Facility=WellFacilitylink.Facility AND WellFacilitylink.WellEvent="%s" """
+    facilities = db.select_sql(statement_facilities % wellevent_num)
+    # facilities = db.select("WellFacilitylink", WellEvent=wellevent_num)
+    return render_template('wellevent/details.html', wellevent=wellevent, leases=leases, facilities=facilities)
     # except:
     #     abort(404)
