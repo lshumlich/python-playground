348c1c0defb9e9bb30becaab825b5f070b68ebba
Konstantin Lazutin <konstantin.lazutin@thesolutionstack.com>
2016-05-05 15:59:35 -0600

    Small updates to map before we move on.

--- calcroyalties/src/app/views/main.py
+++ calcroyalties/src/app/views/main.py
@@ -38,8 +38,7 @@ def logout():
     if 'login' in session:
         session.clear()
         resp.set_cookie('proddate', expires=0)
-    return resp
-    # return redirect(url_for('index'))
+    return resp  # used instead of regular redirect because set_cookie only works on a custom response
 
 @app.route('/map')
 def map():
@@ -57,9 +56,8 @@ def map():
         json_obj['properties']['popupContent'] = '<b>%s</b> <br> Pool Name: %s<br><a href="/wellevent/%s">Details</a>' % (wellevent.WellEvent, wellevent.PoolName, wellevent.WellEvent)
         json_obj['geometry'] = {}
         json_obj['geometry']['type'] = 'Point'
-        json_obj['geometry']['coordinates'] = [random.randint(1, 50), random.randint(1, 100)]
+        json_obj['geometry']['coordinates'] = [random.randint(-50, 50), random.randint(-100, 100)]
         results.append(json_obj)
-    print(results)
     return render_template('map.html', results=json.dumps(results))
 
 @app.errorhandler(404)
@@ -79,42 +77,7 @@ def get_proddate_int():
         raise AppError('Proddate cookie not set for some reason')
 
 """
-V 1. Authentication system
-    V a. Checkboxes for permissions (names like well_edit, on server added to a list if checked, then list = permissions in db)
-    V b. JS-based form verification before submitting (use jQuery dialog demo for example)
-    V c. If permissions not set, default them to Disabled
-2. Well event view
-    a. Search by well event only
-    b. Search results based on Lorraine's table
-    c. Details page - show entire record
-    d. Details page - show related wells
-    e. Search fields verification; get rid of empty fields in JS
-3. Lease view
-    V a. Start by designing url structure!
-    b. No search for now
-    c. Search results - show entire record
-    d. Details page - show entire record
-    e. Details page - show related wells
-3. Larry's data view
-    a. Make sure it's working
-4. Production month
-    a. Store in cookies, make sure persistent for user on this computer
-    b. Prompt a user if it's not set
-    c. Write a function that would get it consistently
-5. Misc
-    a. Implement flash messages (how to make it work from JS?)
-    b. Change all endpoints to make use of GET parameters: /lease/search?ID=1&Prov=AB, then populate the form if these were passed on:
-       value = {{ lease.ID or "" }}
-
-Questions to Larry:
-1. select_sql when expecting just one result? if result.len==1 return result[0]?
-2. No leases in Lease table matching the WellLeaseLink lookup results
-3. Almost no results when taking into account production date.
-
-1. Try outer joins for dates to have fields empty where date doesn't match
-2. If available, include results from Well in WellEvent details
-3. Change the Facility details view to a separate page a-lá WellEvent
-4. Change the proddate modal to update text instantly
-
-Idea: change all db.select functions to take into account the production date (to avoid setting it manually on every query)
+1. Well event view - search field verification
+2. Make flash messages work from JS (i.e. on AJAX request status)
+3. Idea: change all db.select functions to take into account the production date (to avoid setting it manually on every query)
 """
