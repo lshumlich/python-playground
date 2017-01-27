f2437ed0afde91449925ddcf1a28e6f8df4e6cb2
Konstantin Lazutin <konstantin.lazutin@thesolutionstack.com>
2017-01-26 21:09:53 -0700

    Initial uploading of Excel worksheets by the user.

--- calcroyalties/src/app/views/process.py
+++ calcroyalties/src/app/views/process.py
@@ -1,9 +1,8 @@
-from flask import Blueprint, request, render_template, abort
+from flask import Blueprint, request, render_template
 
-import config
-from .permission_handler import PermissionHandler
-from .main import get_proddate_int
+from batch import drop_create_tables, process_royalties
 from src.calc import volumetric_to_monthly
+from src.tool import sqlite_load_excel
 
 process = Blueprint('process', __name__)
 
@@ -11,3 +10,20 @@ process = Blueprint('process', __name__)
 # @PermissionHandler('well_view')
 def load_petrinex():
     return(volumetric_to_monthly.volumetric_to_monthly())
+
+@process.route('/process/load_xls', methods=['GET', 'POST'])
+def load_xls():
+    if request.method == 'GET':
+        return render_template('/process/load_xls.html')
+    elif request.method == 'POST':
+        file = request.files['fileToUpload']
+        results = process_xls(file)
+        return render_template('/process/load_xls.html', results=results)
+
+def process_xls(file):
+    drop_create_tables()
+    sqlite_load_excel.load_sheet(file)
+    process_royalties()
+    results = 'File <b>%s</b> processed successfully.' % file.filename
+    print(results)
+    return results
