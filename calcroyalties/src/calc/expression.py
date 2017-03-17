2d87114c4f28617c041fef402bddf0c0790f724f
lshumlich <larry.shumlich@thesolutionstack.com>
2017-03-17 08:18:35 -0600

    - Begin Lookup feature in an expression

--- calcroyalties/src/calc/expression.py
+++ calcroyalties/src/calc/expression.py
@@ -1,5 +1,6 @@
 """
-This class is intended to evaluate expressions that are needed in the Royalty Calculation.
+This generic class is intended to evaluate expressions that are needed in the Royalty Calculation. The expressions are
+expected is a format like: "=(prod + 10 - (sales * 3))". The expression itself can be anywhere in the string.
 
 One place where this will be used is in the royalty calculation of a GORR. The GORR requires a formula to be
 done based on monthly input. The formula expression uses a python package called py-expression-eval.
@@ -24,3 +25,51 @@ Look at Sphinx for documentation generation.
 
 """
 
+from py_expression_eval import Parser
+from src.util.apperror import AppError
+
+
+class Expression():
+
+    def find_expression(selfs, s):
+        a = s.find('=(')
+        if a < 0:
+            raise AppError('No formula found in string: ' + s)
+        brace_count = 1
+        for i in range(a + 2, len(s)):
+            if s[i] == ')' : brace_count -= 1
+            if s[i] == '(' : brace_count += 1
+            if brace_count == 0 : break
+
+        if brace_count != 0:
+            raise AppError('The formula was not ended. Looking for matching ending ")" in string: ' + s)
+
+        return a, i
+
+    def get_expression(self, s):
+        b, e = self.find_expression(s)
+        return s[b+2:e]
+
+    def evaluate_expression(self, s, m):
+        e = self.get_expression(s)
+        parse = Parser()
+        expr = parse.parse(e)
+        print("tostring : ", expr.toString())
+        print("variables: ", expr. variables())
+
+    def resolve_expression(self, s, m):
+        return "asdf"
+
+    def lookup(self, var_list, monthly):
+        var_values = {}
+        for v in var_list:
+            if v == "sales":
+                var_values[v] = monthly.SalesVol
+            elif v == "prod":
+                var_values[v] = monthly.ProdVol
+            elif v == "gj":
+                var_values[v] = monthly.GJ
+            else:
+                var_values[v] = "wtf"
+            print(v)
+        return var_values
--- calcroyalties/tests/calc/expression_test.py
+++ calcroyalties/tests/calc/expression_test.py
@@ -1,9 +1,66 @@
 import unittest
 from py_expression_eval import Parser
 
-class TestSaskRoyaltyCalc(unittest.TestCase):
+from src.calc.expression import Expression
+from src.util.apperror import AppError
+from src.database.data_structure import DataStructure
+
+
+class TestExpresion(unittest.TestCase):
 
     def test_stuff(self):
         parser = Parser()
-        v = parser.parse('m.myvalue * m.yourvalue').evaluate({"m.myvalue":3, 'm.yourvalue':4})
+        v = parser.parse('m.myvalue * m.yourvalue').evaluate({"m.myvalue": 3, 'm.yourvalue': 4})
         print(v)
+
+    def test_find_expression(self):
+        expression = Expression()
+
+        s = "no formula in here"
+        self.assertRaises(AppError, expression.find_expression, s) # Note there is no expression
+
+        s = "asdf =(abc + (def * 3)) more stuff in here"
+        self.assertEqual((5, 22), expression.find_expression(s))
+
+        s = "=(abc + (def * 3)) more stuff in here"
+        self.assertEqual((0, 17), expression.find_expression(s))
+
+        s = "=(abc + (def * 3))"
+        self.assertEqual((0, 17), expression.find_expression(s))
+
+        s = "asdf =(abc + (def * 3) more stuff in here" # Note the expression does not end
+        self.assertRaises(AppError, expression.find_expression, s)
+
+    def test_get_expression(self):
+        expression = Expression()
+
+        s = "no formula in here"
+        self.assertRaises(AppError, expression.get_expression, s) # Note there is no expression
+
+        s = "asdf =(abc + (def * 3)) more stuff in here"
+        self.assertEqual("abc + (def * 3)", expression.get_expression(s))
+
+    def test_evaluate_expression(self):
+        expression = Expression()
+
+        m = DataStructure()
+        m.ProdMonth = 201703
+        m.ProdVol = 100
+        m.SalesVol = 90
+        m.GJ = 1000
+
+        s = "asdf =(prod + sales + gj) more stuff in here"
+        self.assertEqual("abc + (def * 3)", expression.evaluate_expression(s, m))
+
+    def test_var_list(self):
+        expression = Expression()
+
+        m = DataStructure()
+        m.ProdMonth = 201703
+        m.ProdVol = 100
+        m.SalesVol = 90
+        m.GJ = 1000
+
+        rv = expression.lookup({"prod", "sales", "gj", "somevalue", "m.somevalue" }, m)
+
+        print(rv)
