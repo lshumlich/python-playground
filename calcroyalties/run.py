fbb51bdc00a4072f35f4bd3500885bd8de5da52f
Adrienne Shumlich <adrienne.shumlich@gmail.com>
2017-05-31 15:33:56 -0600

    Run updates you sent via email

--- calcroyalties/run.py
+++ calcroyalties/run.py
@@ -1,9 +1,17 @@
-#!/bin/env python3
+# !/bin/env python3
 import logging, sys
 
 root = logging.getLogger()
 root.setLevel(logging.INFO)
 
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
+else:
+    port = 5000
+
+print("Starting on port:", port)
+
 if __name__ == '__main__':
     from src.app import app
-    app.run('0.0.0.0', debug=True)
+
+    app.run('0.0.0.0', port=port, debug=True)
